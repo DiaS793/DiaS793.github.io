b0a0d42e173be5e534cd59c74ca4eb680143bd9d
DiaS793 <DiaScruggs793@outlook.com>
2022-03-18 02:29:00 -0400

    3/18/22 FINALLYgit add .!

--- index.js
+++ index.js
@@ -7,50 +7,41 @@ const btn = document.getElementById("mybtn");
 const btn2 = document.getElementById("mybtn2");
 const btn3 = document.getElementById("mybtn3");
 const btn4 = document.getElementById("mybtn4");
-const span = document.getElementsByClassName("close")[0];
+const  span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function () {
   modal.style.display = "block";
 };
 
 btn2.onclick = function () {
-  modal2.style.display = "block";
+   
+  modal.style.display = "block";
 };
 
 btn3.onclick = function () {
-  modal3.style.display = "block";
+  modal.style.display = "block";
 };
 
 btn4.onclick = function () {
-  modal4.style.display = "block";
+  modal.style.display = "block";
 };
 
-span.onclick = function () {
+span.onclick = function() {
+    console.log("hey,hey");
   modal.style.display = "none";
 };
 
-span.onclick = function () {
-  modal2.style.display = "none";
-};
 
-span.onclick = function () {
-  modal3.style.display = "none";
-};
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+// //   } else if (event.target == modal2) {
+// //     modal.style.display = "none";
+// //   } else if (event.target == modal3) {
+// //     modal.style.display = "none";
+// //   } else if (event.target == modal4) {
+// //     modal.style.display = "none";
+//   }
+// };
 
-span.onclick = function () {
-  modal4.style.display = "none";
-};
 
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  } else if (event.target == modal2) {
-    modal.style.display = "none";
-  } else if (event.target == modal3) {
-    modal.style.display = "none";
-  } else if (event.target == modal4) {
-    modal.style.display = "none";
-  }
-};
-
-window.onclick = function (event) {};
